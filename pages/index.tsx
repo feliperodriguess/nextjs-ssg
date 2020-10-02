4397e8e237dbf9cc86ee5f66a56503ff4aeac5c6
Felipe Augusto <felipe.augusto.rodrigues1@gmail.com>
2020-10-02 18:39:40 -0300

    feat: Generate static page with SWR strategy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { GetStaticProps } from 'next';
+
+export default function Home({ org }) {
+  return (
+    <div>
+      <h1>{org.login}</h1>
+      <h2>{org.description}</h2>
+
+      <p>
+        Site: <a href={org.blog}>{org.blog}</a>
+      </p>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://api.github.com/orgs/jungledevs');
+  const data = await response.json();
+
+  return {
+    props: {
+      org: data,
+    },
+    revalidate: 30,
+  };
+};
